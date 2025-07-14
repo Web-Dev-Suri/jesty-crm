cecb739e512922196a19ac23c645ca8b83f6bb97
Web-Dev-Suri <harshitsr01@gmail.com>
2025-07-14 13:57:36 +0530

    api changes

--- frontend/src/config/serverApiConfig.js
+++ frontend/src/config/serverApiConfig.js
@@ -1,7 +1,7 @@
 export const API_BASE_URL =
   import.meta.env.PROD || import.meta.env.VITE_DEV_REMOTE == 'remote'
     ? import.meta.env.VITE_BACKEND_SERVER + 'api/'
-    : 'http://3.109.229.73:8888/';
+    : 'http://3.109.229.73:8888/api/';
 export const BASE_URL =
   import.meta.env.PROD || import.meta.env.VITE_DEV_REMOTE
     ? import.meta.env.VITE_BACKEND_SERVER
